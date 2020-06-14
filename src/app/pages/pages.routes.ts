56526125a607d01ff5f01811b986eeb9eb901ef8
Mario ALvarez <alvarezsotomario@gmail.com>
2020-06-13 18:35:23 -0600

    agregar y modificar modulos

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,22 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graficas ', component: Graficas1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+    ]
+  }
+];
+
+export const PAGES_ROUTES = RouterModule.forRoot(routes, { useHash: true });
