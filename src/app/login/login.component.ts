efc3ffe9261dfecd8189426605f5691fd4fe0606
Mario ALvarez <alvarezsotomario@gmail.com>
2020-06-17 18:39:52 -0600

    Servicios, temas, rutas, sidebar

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+declare function init_plugins();
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( public router: Router ) { }
 
   ngOnInit(): void {
+    init_plugins();
+  }
+
+  ingresar(){
+    this.router.navigate([ '/dashboard' ]);
   }
 
 }
